9df1906971e9168f4a1eae2941b857d9bb83961c
Jonathan Chou <jonathanchou@Jonathans-MacBook-Pro-2.local>
2014-10-30 16:09:59 -0700

    Initial Commit

---
+++ LionsAndTigers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LionsAndTigers
+//
+//  Created by Jonathan Chou on 10/30/14.
+//  Copyright (c) 2014 Jonathan Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LionsAndTigers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LionsAndTigers
+//
+//  Created by Jonathan Chou on 10/30/14.
+//  Copyright (c) 2014 Jonathan Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
